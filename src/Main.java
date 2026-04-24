a9a9e23839b98ab4b10b9952fb98fd7bab48ef1a
Ojas Mutreja <118201856+HelloOjasMutreja@users.noreply.github.com>
2026-04-24 09:03:35 +0530

    UC16: Sort Passenger Bogies by Capacity
    Removed CargoSafetyException and GoodsBogie class. Implemented sorting of capacities in main method.

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
-
-    static class CargoSafetyException extends RuntimeException {
-        CargoSafetyException(String msg) {
-            super(msg);
-        }
-    }
-
-    static class GoodsBogie {
-        String type;
-        String cargo;
-
-        void assignCargo(String type, String cargo) {
-            this.type = type;
-            if (type.equals("Rectangular") && cargo.equals("Petroleum")) {
-                throw new CargoSafetyException("Unsafe cargo assignment!");
-            }
-            this.cargo = cargo;
-        }
-    }
 
     public static void main(String[] args) {
 
-        GoodsBogie bogie = new GoodsBogie();
+        int[] capacities = {72, 56, 24, 70};
 
-        try {
-            bogie.assignCargo("Rectangular", "Petroleum");
-            System.out.println("Cargo assigned");
-        } catch (CargoSafetyException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("Operation completed.");
+        for (int i = 0; i < capacities.length - 1; i++) {
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
         }
+
+        System.out.println("Sorted Capacities: " + Arrays.toString(capacities));
     }
 }
